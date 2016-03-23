decd137fefa173d03496fc71506992b4721398b6
Wesley Haws <wesleyhaws1@gmail.com>
2016-03-23 12:15:38 -0600

    Added files via upload

--- windowsSystemInfo.py
+++ windowsSystemInfo.py
@@ -1,6 +1,9 @@
 import socket,struct
 import os
 import subprocess
+import platform 
+from ctypes import *
+from ctypes.wintypes import *
 
 def get_netmask(ip):
 	proc = subprocess.Popen('ipconfig',stdout=subprocess.PIPE)
@@ -10,17 +13,20 @@ def get_netmask(ip):
 			break
 	mask = proc.stdout.readline().rstrip().split(b':')[-1].replace(b' ',b'').decode()
 	return mask
-
-def _os_version():
 	
+def os_bit_version():
+	if os.environ['PROGRAMFILES(X86)']:
+		return "64-Bit"
+	else:
+		return "32-Bit"
 
 def main():
 	os.system('cls')
-	print ("-"*20,"Gathering Information","-"*20)
+	print ("-"*20,"Computer Information","-"*20)
 	print ("Computer Name","."*16,socket.gethostname())
 	internal_ip = socket.gethostbyname(socket.gethostname())
 	print ("IPAddress","."*20,internal_ip)
 	print ("Subnet Mask","."*18,get_netmask(internal_ip))
-	print ("OS Version",_os_version())
+	print ("OS Type","."*22,os_bit_version())
 
 main()
